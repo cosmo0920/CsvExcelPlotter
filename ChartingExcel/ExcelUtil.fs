1bd3e47ea7dd619f8657ec1a08c58b3ee996e33e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-03-17 23:57:00 +0900

    Split modules

---
+++ ChartingExcel/ExcelUtil.fs
@@ -0,0 +1,17 @@
+﻿module public Excel.Util
+open Microsoft.Office.Interop.Excel
+
+// 範囲の周りのセルに罫線を設定します。
+let SetPattern (range:Range) =
+    range.Borders.[XlBordersIndex.xlEdgeLeft].LineStyle <- XlLineStyle.xlContinuous
+    range.Borders.[XlBordersIndex.xlEdgeLeft].Weight <- XlBorderWeight.xlThin
+    range.Borders.[XlBordersIndex.xlEdgeTop].LineStyle <- XlLineStyle.xlContinuous
+    range.Borders.[XlBordersIndex.xlEdgeTop].Weight <- XlBorderWeight.xlThin
+    range.Borders.[XlBordersIndex.xlEdgeBottom].LineStyle <- XlLineStyle.xlContinuous
+    range.Borders.[XlBordersIndex.xlEdgeBottom].Weight <- XlBorderWeight.xlThin
+    range.Borders.[XlBordersIndex.xlEdgeRight].LineStyle <- XlLineStyle.xlContinuous
+    range.Borders.[XlBordersIndex.xlEdgeRight].Weight <- XlBorderWeight.xlThin
+
+// 上下左右を罫線で囲う
+let SimplePattern (range:Range) =
+    range.Borders.LineStyle <- XlLineStyle.xlContinuous
