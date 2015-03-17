358ba38c591ad1b5384e09c897ffc5d8aa781474
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-03-17 22:22:47 +0900

    Refactoring.

--- ChartingExcel/Program.fs
+++ ChartingExcel/Program.fs
@@ -18,35 +18,6 @@ let workbook = app.Workbooks.Add(XlWBATemplate.xlWBATWorksheet)
 // Note that worksheets are indexed from one instead of zero
 let worksheet = (workbook.Worksheets.[1] :?> Worksheet)
 
-let readCsvData (csvData: Runtime.CsvFile<CsvRow>) =
-    [|for row in csvData.Rows do
-        yield [|row.GetColumn(column1); row.GetColumn(column2); row.GetColumn(column3)|]|]
-let readCsvRowName (csvData: Runtime.CsvFile<CsvRow>) =
-    [|for row in csvData.Rows do
-        yield [|row.GetColumn(rowName); |]|]
-
-let processCsv csvData =
-    match csvData with
-    | Some(dataFile) -> let csv = readCsvData dataFile
-                        let rowItem = readCsvRowName dataFile
-                        let length = csv.Length
-                        // Store data in arrays of strings or floats
-                        let titles = [| column1; column2; column3 |]
-                        let titleLength = titles.Length
-                        let names = Array2D.init 10 1 (fun i _ -> rowItem.[i].[0]) //string('A' + char(i)))
-                        let data = Array2D.init length titleLength (fun i j -> csv.[i].[j])
-                        worksheet.Range("C2", "E2").Value2 <- titles
-                        worksheet.Range("B3", "B12").Value2 <- names
-                        worksheet.Range("C3", "E12").Value2 <- data
-    | None -> do System.Environment.Exit 1
-
-processCsv dataFile
-worksheet.Columns.Range("A:A").ColumnWidth <- leftSpaceWidth
-
-worksheet.Name <- sheet1Name
-let range = worksheet.Range("B2", "E2")
-let range2 = worksheet.Range("B2:B12")
-let range3 = worksheet.Range("C3:E12")
 // 範囲の周りのセルに罫線を設定します。
 let SetPattern (range:Range) =
     range.Borders.[XlBordersIndex.xlEdgeLeft].LineStyle <- XlLineStyle.xlContinuous
@@ -62,9 +33,41 @@ let SetPattern (range:Range) =
 let SimplePattern (range:Range) =
     range.Borders.LineStyle <- XlLineStyle.xlContinuous
 
-let SetBorders = SetPattern range
-let SetBorders2 = SimplePattern range2
-let SetBorders3 = SetPattern range3
+let readCsvData (csvData: Runtime.CsvFile<CsvRow>) =
+    [|for row in csvData.Rows do
+        yield [|row.GetColumn(column1); row.GetColumn(column2); row.GetColumn(column3)|]|]
+let readCsvRowName (csvData: Runtime.CsvFile<CsvRow>) =
+    [|for row in csvData.Rows do
+        yield [|row.GetColumn(rowName); |]|]
+
+let processCsv csvData =
+    match csvData with
+    | Some(dataFile) -> let csv = readCsvData dataFile
+                        let rowItem = readCsvRowName dataFile
+                        let length = csv.Length
+                        // Store data in arrays of strings or floats
+                        let titles = [| column1; column2; column3 |]
+                        let titleLength = titles.Length
+                        let names = Array2D.init length 1 (fun i _ -> rowItem.[i].[0])
+                        let data = Array2D.init length titleLength (fun i j -> csv.[i].[j])
+                        let titleArea = "C2:" + string('C'+char(titleLength-1))+"2"
+                        let namesArea = "B3:B"+string(3+length-1)
+                        let rowsArea = "C3:E"+string(3+length-1)
+                        worksheet.Range(titleArea).Value2 <- titles
+                        worksheet.Range(namesArea).Value2 <- names
+                        worksheet.Range(rowsArea).Value2 <- data
+                        worksheet.Name <- sheet1Name
+                        let titleRange = worksheet.Range(titleArea)
+                        let range2 = worksheet.Range("B2:B"+string(3+length-1))
+                        let rowsRange = worksheet.Range(rowsArea)
+
+                        let SetBorders = SetPattern titleRange
+                        let SetBorders2 = SimplePattern range2
+                        let SetBorders3 = SetPattern rowsRange
+                        worksheet.Columns.Range("A:A").ColumnWidth <- leftSpaceWidth
+    | None -> do System.Environment.Exit 1
+
+processCsv dataFile
 
 [<EntryPoint>]
 let main argv = 
