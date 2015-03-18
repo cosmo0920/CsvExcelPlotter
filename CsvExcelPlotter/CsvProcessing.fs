8f502455e947a89621f180ab27946f6a3a0ba8bb
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-03-18 23:18:10 +0900

    Split schema definition into module

--- CsvExcelPlotter/CsvProcessing.fs
+++ CsvExcelPlotter/CsvProcessing.fs
@@ -1,22 +1,9 @@
 ﻿module CsvProcessing
 
 open Microsoft.Office.Interop.Excel
-open System
 open FSharp.Data
-open System.IO
-open Excel.Util
 open ProcessedCsvType
-
-let rowName = {Name = "itemName"}
-let columns = {Names = [|"item1"; "item2"; "item3"|]}
-let column1 = "item1"
-let column2 = "item2"
-let column3 = "item3"
-
-let readCsvData (csvData: Runtime.CsvFile<CsvRow>) =
-    [|for row in csvData.Rows -> [|row.[columns.Names.[0]]; row.[columns.Names.[1]]; row.[columns.Names.[2]];|]|]
-let readCsvRowName (csvData: Runtime.CsvFile<CsvRow>) =
-    [|for row in csvData.Rows -> [|row.[rowName.Name]; |]|]
+open CsvSchema
 
 let processCsv csvData =
     match csvData with
@@ -24,7 +11,6 @@ let processCsv csvData =
                         let rowItem = readCsvRowName dataFile
                         let length = csv.Length
                         // Store data in arrays of strings or floats
-                        let titles = [| column1; column2; column3 |]
                         let titleLength = titles.Length
                         let names = Array2D.init length 1 (fun i _ -> rowItem.[i].[0])
                         let data = Array2D.init length titleLength (fun i j -> csv.[i].[j])
---
+++ CsvExcelPlotter/CsvSchema.fs
@@ -0,0 +1,16 @@
+﻿module CsvSchema
+
+open Microsoft.Office.Interop.Excel
+open FSharp.Data
+open ProcessedCsvType
+
+let rowName = "itemName"
+let column1 = "item1"
+let column2 = "item2"
+let column3 = "item3"
+let titles = [| column1; column2; column3 |]
+
+let readCsvData (csvData: Runtime.CsvFile<CsvRow>) =
+    [|for row in csvData.Rows -> [|row.[column1]; row.[column2]; row.[column3];|]|]
+let readCsvRowName (csvData: Runtime.CsvFile<CsvRow>) =
+    [|for row in csvData.Rows -> [|row.[rowName]; |]|]
