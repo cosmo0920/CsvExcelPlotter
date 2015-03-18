95b2d3816e0dd57f9cc5d5f53e16a947a572e295
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-03-18 23:19:36 +0900

    Remove unused user defined types

--- CsvExcelPlotter/ProcessedCsvType.fs
+++ CsvExcelPlotter/ProcessedCsvType.fs
@@ -3,5 +3,3 @@
 open FSharp.Data
 
 type ProcessedCsv = {Titles:string []; Names: string[,]; CsvData: string [,]; RowLength: int}
-type TargetRowName = {Name: string}
-type TargetColumnNames = {Names: string []}
